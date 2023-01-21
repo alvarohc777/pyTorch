876303e337105e686125ee7e43ebffac5068898e
alvarohc777 <aherrada@uninorte.edu.co>
2023-01-21 17:21:18 -0500

    Add sine + harmonic function for dataset creation
    from utils module, added sine_harmonic() and sine_harmonics()
    sine_harmonics() uses sine_harmonic() to create dataset.

--- utils/sine_dataset.py
+++ utils/sine_dataset.py
@@ -4,6 +4,26 @@ import matplotlib.pyplot as plt
 from random import getrandbits
 
 
+def sine_harmonic(t, fs=3480, N=64, fundamental=60, harmonic=1, magnitude=1, phi=0):
+    wt = 2 * np.pi * fs * t
+    n = harmonic
+    phi = phi
+    phi_a = float(0) * np.pi / 180
+    phi_a = phi_a + phi
+    signal = magnitude * np.sin(n * (wt + phi_a))
+    return signal
+
+
+def sine_harmonics(signals, target, t, fs, m, mag_i=0.01, mag_f=100):
+    for magnitude in np.linspace(mag_i, mag_f, int(m / 2)):
+        phi = np.random.normal(-np.pi, np.pi)
+        harmonic = np.random.normal(1, 8)
+        signal = sine_harmonic(t, phi=phi, harmonic=harmonic, magnitude=magnitude)
+        signals = np.vstack((signals, signal))
+        target = np.vstack((target, np.array(1)))
+    return signals, target
+
+
 def sine_creator(signals, target, t, fs, m, mag_i=0.01, mag_f=100):
     for magnitude in np.linspace(mag_i, mag_f, int(m / 2)):
         signal = np.array(magnitude * np.sin(2 * np.pi * fs * t))
@@ -21,6 +41,7 @@ def sine_phase_creator(signals, target, t, fs, m, mag_i=0.01, mag_f=100):
     return signals, target
 
 
+# Crear esto como una transformación para los datos de entrada
 def sine_noise(signals, target, t, fs, m, mag_i=0.01, mag_f=100):
     for magnitude in np.linspace(mag_i, mag_f, int(m / 2)):
         x = np.random.uniform()
@@ -31,6 +52,33 @@ def sine_noise(signals, target, t, fs, m, mag_i=0.01, mag_f=100):
         signals = np.vstack((signals, signal + gauss))
         target = np.vstack((target, np.array(1)))
     return signals, target
+
+
+def synthetic_signal(t, harmonics=[60], fundamental=60):
+    """
+    Function for the creation of a synthetic signal
+
+
+
+    Returns
+    -------
+    numpy.array
+        (3, n) three phase matrix
+    """
+    wt = 2 * np.pi * fundamental * t
+    ang_A = float(0) * np.pi / 180
+    ang_B = float(240) * np.pi / 180
+    ang_C = float(120) * np.pi / 180
+
+    A, B, C = 0, 0, 0
+
+    for s in harmonics:
+        n = s / fundamental
+        A += 100 * np.sin(n * (wt + ang_A))
+        B += 100 * np.sin(n * (wt + ang_B))
+        C += 100 * np.sin(n * (wt + ang_C))
+
+    return np.array((A, B, C))
 
 
 def gaussian_creator(
