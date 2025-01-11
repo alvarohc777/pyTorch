6106da787e59ba33b39b46510dc3c9bcbc3e31d5
alvarohc777 <aherrada@uninorte.edu.co>
2025-01-11 13:40:30 -0500

    extraer lista de señales a un módulo aparte

---
+++ utils/relay_list.py
@@ -0,0 +1,115 @@
+# Load Dataset
+current_R1A = "I: X0039A-R1A"
+current_R1B = "I: X0039B-R1B"
+current_R1C = "I: X0039C-R1C"
+current_R2A = "I: X0001A-R2A"
+current_R2B = "I: X0001B-R2B"
+current_R2C = "I: X0001C-R2C"
+current_R3A = "I: X0001A-R3A"
+current_R3B = "I: X0001B-R3B"
+current_R3C = "I: X0001C-R3C"
+current_R4A = "I: X0038A-R4A"
+current_R4B = "I: X0038B-R4B"
+current_R4C = "I: X0038C-R4C"
+current_R5A = "I: X0038A-R5A"
+current_R5B = "I: X0038B-R5B"
+current_R5C = "I: X0038C-R5C"
+current_R6A = "I: X0002A-R6A"
+current_R6B = "I: X0002B-R6B"
+current_R6C = "I: X0002C-R6C"
+current_R7A = "I: X0002A-R7A"
+current_R7B = "I: X0002B-R7B"
+current_R7C = "I: X0002C-R7C"
+current_R8A = "I: X0003A-R8A"
+current_R8B = "I: X0003B-R8B"
+current_R8C = "I: X0003C-R8C"
+current_R9A = "I: X0003A-R9A"
+current_R9B = "I: X0003B-R9B"
+current_R9C = "I: X0003C-R9C"
+current_R10A = "I: X0004A-R10A"
+current_R10B = "I: X0004B-R10B"
+current_R10C = "I: X0004C-R10C"
+current_R11A = "I: X0004A-R11A"
+current_R11B = "I: X0004B-R11B"
+current_R11C = "I: X0004C-R11C"
+current_R12A = "I: X0005A-R12A"
+current_R12B = "I: X0005B-R12B"
+current_R12C = "I: X0005C-R12C"
+current_R13A = "I: X0005A-R13A"
+current_R13B = "I: X0005B-R13B"
+current_R13C = "I: X0005C-R13C"
+current_R14A = "I: X0031A-R14A"
+current_R14B = "I: X0031B-R14B"
+current_R14C = "I: X0031C-R14C"
+current_R15A = "I: X0031A-R15A"
+current_R15B = "I: X0031B-R15B"
+current_R15C = "I: X0031C-R15C"
+current_R16A = "I: X0045A-R16A"
+current_R16B = "I: X0045B-R16B"
+current_R16C = "I: X0045C-R16C"
+current_R17A = "I: X0032A-R17A"
+current_R17B = "I: X0032B-R17B"
+current_R17C = "I: X0032C-R17C"
+current_R18A = "I: X0032A-R18A"
+current_R18B = "I: X0032B-R18B"
+current_R18C = "I: X0032C-R18C"
+
+signals = [
+    current_R1A,
+    current_R1B,
+    current_R1C,
+    current_R2A,
+    current_R2B,
+    current_R2C,
+    current_R3A,
+    current_R3B,
+    current_R3C,
+    current_R4A,
+    current_R4B,
+    current_R4C,
+    current_R5A,
+    current_R5B,
+    current_R5C,
+    current_R6A,
+    current_R6B,
+    current_R6C,
+    current_R7A,
+    current_R7B,
+    current_R7C,
+    current_R8A,
+    current_R8B,
+    current_R8C,
+    current_R9A,
+    current_R9B,
+    current_R9C,
+    current_R10A,
+    current_R10B,
+    current_R10C,
+    current_R11A,
+    current_R11B,
+    current_R11C,
+    current_R12A,
+    current_R12B,
+    current_R12C,
+    current_R13A,
+    current_R13B,
+    current_R13C,
+    current_R14A,
+    current_R14B,
+    current_R14C,
+    current_R15A,
+    current_R15B,
+    current_R15C,
+    current_R16A,
+    current_R16B,
+    current_R16C,
+    current_R17A,
+    current_R17B,
+    current_R17C,
+    current_R18A,
+    current_R18B,
+    current_R18C,
+]
+
+if __name__ == "__main__":
+    print(__name__)
